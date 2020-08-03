871ee1b905b0b435ff59d69110cfa5a45d6fe83f
Savannah Vaith <sav.vaithilingam@gmail.com>
2020-08-03 11:07:21 +0100

    Added floating test

--- __tests__/jestTest.js
+++ __tests__/jestTest.js
@@ -51,7 +51,8 @@ describe("Numbers", () => {
 
     test("Adding floating numbers", () => {
         const value = 0.1 + 0.2;
-        expect(value).toBe(0.3);
+        // expect(value).toBe(0.3); this won't work because of rounding error
+        expect(value).toBeCloseTo(0.3);
     })
 })
 
