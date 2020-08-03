41340e09fce9a75ab2bbe7d01ea1d1cfb54668e8
Savannah Vaith <sav.vaithilingam@gmail.com>
2020-08-03 10:48:09 +0100

    added object test

--- __tests__/jestTest.js
+++ __tests__/jestTest.js
@@ -1,5 +1,11 @@
-describe("Filter function", () =>{
-    test("expect 2+2 to be 4", () =>{
-        expect(2+2).toBe(4);
-    })
+describe("Common Matchers", () => {
+    test("Expect 10+9 to be 19", () => {
+        expect(10 + 9).toBe(19);
+    });
+
+    test("Object assignment", () => {
+        const data = { ham: true };
+        data["cheese"] = false;
+        expect(data).toEqual({ ham: true, cheese: false });
+    });
 });
