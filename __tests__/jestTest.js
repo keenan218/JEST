17689c31cd7ee17992778cbd2476a808f27101e6
Savannah Vaith <sav.vaithilingam@gmail.com>
2020-08-03 11:03:25 +0100

    Added number tests

--- __tests__/jestTest.js
+++ __tests__/jestTest.js
@@ -37,4 +37,16 @@ describe("Truthiness", () => {
     });
 })
 
+describe("Numbers", () => {
+    test("Matcher Equivalent", () => {
+        const value = 2+2; 
+        expect(value).toBeGreaterThan(3);
+        expect(value).toBeGreaterThanOrEqual(3.5);
+        expect(value).toBeLessThan(5);
+        expect(value).toBeLessThanOrEqual(4.5);
+
+        expect(value).toBe(4);
+        expect(value).toEqual(4);
+    })
+})
 
