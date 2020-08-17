44c494da12c35c4e481b5e148427443e22045597
Savannah Vaith <sav.vaithilingam@gmail.com>
2020-08-17 16:57:51 +0100

    Infinite game logic added

---
+++ 7 - Timer mocks/infinitetimergame.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const infinitetimergame = callback => {
+    console.log("Ready... Go!");
+    setTimeout(()=> {
+        console.log("Times up - 10 seconds before the next game starts");
+        callback && callback();
+
+        //Schedule the next game in 10 seconds
+        setTimeout(()=>{
+            infinitetimergame(callback);
+        },10000);
+    },10000);
+}
+
+infinitetimergame(()=>console.log("hi"));
+
+module.exports = infinitetimergame;
