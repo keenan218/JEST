641977398d8b76850bd83d18c9b4544a0f609541
Savannah Vaith <sav.vaithilingam@gmail.com>
2020-08-17 15:19:38 +0100

    Added multiple property types to example

--- 6 - Deep snapshot testing/multiple_prop_datatypes/src/App.js
+++ 6 - Deep snapshot testing/multiple_prop_datatypes/src/App.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ComponentWithProps from './ComponentWithProps';
 
 function App() {
+  const props = {
+    headerText: `A header`,
+    contentText: `Some content`,
+    numericProp: 100,
+    expressionProp: 2 + 2,
+    valueProp: 1000,
+    objectProp: {
+      key1: `key 1 value`,
+      key2: 2,
+      key3: [`Key 3`, `array`, `values`]
+    },
+    arrayProp: [1, 2, 3],
+    functionProp: () => `Hello World`,
+  }
+
   return (
     <div className="App">
-      <ComponentWithProps headerText={"Hello"}/>
+      <ComponentWithProps {...props}/>
     </div>
   );
 }
