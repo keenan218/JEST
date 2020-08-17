7b57a4ffcc4384d103a567a99b059ffd6532fd88
Savannah Vaith <sav.vaithilingam@gmail.com>
2020-08-17 16:54:37 +0100

    Added Timer component

---
+++ 7 - Timer mocks/timergame.js
@@ -0,0 +1,10 @@
+"use strict";
+const timerGame = (callback) => {
+  console.log("Ready... go");
+  setTimeout(() => {
+    console.log("Time's up... stop!");
+    callback && callback();
+  }, 1000);
+};
+
+module.exports = timerGame;
