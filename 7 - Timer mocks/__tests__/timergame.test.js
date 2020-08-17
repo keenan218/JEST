216173fd6d301a71d88ad2bd1f218ad0f1e9e6cf
Savannah Vaith <sav.vaithilingam@gmail.com>
2020-08-17 17:26:41 +0100

    added advance timer by time test

--- 7 - Timer mocks/__tests__/timergame.test.js
+++ 7 - Timer mocks/__tests__/timergame.test.js
@@ -25,3 +25,20 @@ test('calls the callback after 1 second', () => {
     expect(callback).toBeCalled();
     expect(callback).toHaveBeenCalledTimes(1);
 });
+
+it('calls the callback after 1 second via advanceTimersByTime', () => {
+    const timerGame = require('../timerGame');
+    const callback = jest.fn();
+
+    timerGame(callback);
+
+    // At this point in time, the callback should not have been called yet
+    expect(callback).not.toBeCalled();
+
+    // Fast-forward until all timers have been executed
+    jest.advanceTimersByTime(1000);
+
+    // Now our callback should have been called!
+    expect(callback).toBeCalled();
+    expect(callback).toHaveBeenCalledTimes(1);
+});
